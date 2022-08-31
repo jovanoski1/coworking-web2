269732a84af239c3884ed6656d4587b9b3fa367d
mihailjovanoski <mjovanoski1520rn@raf.rs>
2022-08-31 13:53:14 +0200

    header change

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const cors = require('cors');
 const db = require('./queries')
 const port = 3000
 
+app.options('*', cors())
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
