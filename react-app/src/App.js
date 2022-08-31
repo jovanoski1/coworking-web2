3a05ecf5793a6fa25b0e54238345b0363502c15b
RadenkovicZarko <105305032+RadenkovicZarko@users.noreply.github.com>
2022-08-31 23:56:17 +0200

    FrontURLCommit

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [email,setEmail] =useState("");
+  const params = new URLSearchParams(window.location.search);
+
+  const onSubmitForm = async e => {
+    e.preventDefault();
+     console.log(window.location.search)
+     console.log(params.get('hash'));
+    try{
+      
+      const body ={email};
+      const hash=params.get('hash');
+       const response = await fetch("https://coworking-khuti.ondigitalocean.app/api/updateTicket" ,{
+        method: "PUT",
+        headers:{"Content-Type": "application/json"},
+        body: JSON.stringify({hash,email})
+      });
+      console.log(response)
+    }
+    catch(err)
+    {
+      console.error(err.message);
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmitForm}>
+        <label>Email</label>
+        <input type="text" required value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="submit" value="Submit" />
+      </form>
+      
     </div>
   );
 }
