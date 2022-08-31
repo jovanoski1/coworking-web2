dd6cf1875f8ce8d7195f4822a28f5d25001e82aa
mihailjovanoski <mjovanoski1520rn@raf.rs>
2022-08-31 12:31:31 +0200

    header change

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const cors = require('cors');
-app.use(cors());
 const port = 3000
 
 app.use(bodyParser.json())
@@ -12,25 +10,12 @@ app.use(
         extended: true,
     })
 )
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
 
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, token');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     //res.setHeader('Access-Control-Allow-Credentials', true);
-
-//     // Pass to next layer of middleware
-//     next();
-// });
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
